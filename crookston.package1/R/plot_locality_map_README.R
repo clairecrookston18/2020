01b0e60c75e7945ef7d6acc9ba1bf01c30ea615a
Claire Crookston <claire.crookston@Claires-MacBook-Pro-2.local>
2020-11-11 15:00:54 -0600

    First commit of package

---
+++ crookston.package1/R/plot_locality_map_README.R
@@ -0,0 +1,22 @@
+#'plot_locality_map
+
+#'This function plots sample sites and population data onto a map to visualize population 
+#'distributions of Gyrynophilus porphyriticus.
+
+
+#' @param 
+#' @return 
+
+#' @example 
+#' 
+
+
+plot_map <- function(x) {
+  x = measurements
+  pop_coords <- measurements %>% 
+    select(population, long.deg, long.mins, lat.deg, lat.mins)
+  
+  
+  
+}
+
